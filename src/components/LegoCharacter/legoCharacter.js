998ac8c7e1aa92aabed54dd8103eb4e6d29a125c
MCrank <marco_mcse@hotmail.com>
2018-11-10 07:56:56 -0600

    Page Load

--- src/components/LegoCharacter/legoCharacter.js
+++ src/components/LegoCharacter/legoCharacter.js
@@ -1,5 +1,52 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import './legoCharacter.scss';
 
 console.log('hell from LegoCharacter');
+
+const loadHeadCarousel = (headsArr) => {
+  let newHeadString = '';
+  headsArr.forEach((head) => {
+    newHeadString += `
+    <div class="carousel-item" id="${head.id}">
+      <img
+        class="d-block w-100"
+        src="${head.imageUrl}"
+      />
+    </div>
+    `;
+  });
+  $('#head-carousel-inner').append(newHeadString);
+};
+
+const loadTorsoCarousel = (torsosArr) => {
+  let newHeadString = '';
+  torsosArr.forEach((torso) => {
+    newHeadString += `
+    <div class="carousel-item" id="${torso.id}">
+      <img
+        class="d-block w-100"
+        src="${torso.imageUrl}"
+      />
+    </div>
+    `;
+  });
+  $('#torso-carousel-inner').append(newHeadString);
+};
+
+const loadLegCarousel = (legsArr) => {
+  let newHeadString = '';
+  legsArr.forEach((leg) => {
+    newHeadString += `
+    <div class="carousel-item" id="${leg.id}">
+      <img
+        class="d-block w-100"
+        src="${leg.imageUrl}"
+      />
+    </div>
+    `;
+  });
+  $('#leg-carousel-inner').append(newHeadString);
+};
+
+export default { loadHeadCarousel, loadTorsoCarousel, loadLegCarousel };
